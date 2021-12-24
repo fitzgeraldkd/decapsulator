936c555ea444aa140ca7c59fb79557ad06ac0f8a
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-12-23 21:52:53 -0500

    Use settings to store opening/closing string pairs

--- src/extension.ts
+++ src/extension.ts
@@ -9,34 +9,32 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	// console.log('Congratulations, your extension "decapsulator" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+	const delimiter: string = vscode.workspace.getConfiguration('decapsulator').delimiter;
+	const decapsulatorRawPairs: string[] = vscode.workspace.getConfiguration('decapsulator').decapsulatorPairs;
+	const decapsulatorPairs: {[opener: string]: string} = {};
+	decapsulatorRawPairs.forEach(rawPair => {
+		const splitRawPair = rawPair.split(delimiter);
+		if (splitRawPair.length === 2) {
+			decapsulatorPairs[splitRawPair[0]] = splitRawPair[1];
+		}
+	});
 	let disposable = vscode.commands.registerCommand('decapsulator.decapsulate', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		const editor = vscode.window.activeTextEditor;
-
 		if (editor) {
 			let cursorStart = editor.selection.start;
 			let cursorEnd = editor.selection.end;
 			if (cursorEnd.isAfter(cursorStart)) {
-				const pairs: {[key: string]: string} = {
-					'(': ')',
-					'[': ']',
-					'{': '}',
-					'<': '>',
-					"'": "'",
-					'"': '"',
-					'`': '`'
-				};
 				let firstRange = new vscode.Range(cursorStart, cursorStart.translate(0, 1));
 				let lastRange = new vscode.Range(cursorEnd.translate(0, -1), cursorEnd);
 				const firstChar = editor.document.getText(firstRange);
 				const lastChar = editor.document.getText(lastRange);
 				// vscode.window.showInformationMessage(`${firstChar} ${lastChar}`);
-				if (pairs[firstChar] === lastChar) {
+				if (decapsulatorPairs[firstChar] === lastChar) {
 					editor.edit(editBuilder => {
 						editBuilder.delete(lastRange);
 						editBuilder.delete(firstRange);
